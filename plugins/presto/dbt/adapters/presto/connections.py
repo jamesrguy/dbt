4a4f108bc0aa58fc9ab67978353bceaa8db7c8ce
Jacob Beck <jake@fishtownanalytics.com>
2019-01-04 12:59:16 -0700

    implement ConnectionManager methods

--- plugins/presto/dbt/adapters/presto/connections.py
+++ plugins/presto/dbt/adapters/presto/connections.py
@@ -2,6 +2,7 @@ from contextlib import contextmanager
 
 from dbt.adapters.base import Credentials
 from dbt.adapters.sql import SQLConnectionManager
+from pyhive import presto
 
 
 PRESTO_CREDENTIALS_CONTRACT = {
@@ -29,7 +30,7 @@ PRESTO_CREDENTIALS_CONTRACT = {
             'type': 'string',
         },
     },
-    'required': ['database', 'schema', 'host'],
+    'required': ['database', 'schema', 'host', 'port'],
 }
 
 
@@ -45,3 +46,41 @@ class PrestoConnectionManager(SQLConnectionManager):
     ALIASES = {
         'catalog': 'database',
     }
+    @contextmanager
+    def exception_handler(self, sql, connection_name='master'):
+        try:
+            yield
+        # TODO: introspect into `DatabaseError`s and expose `errorName`,
+        # `errorType`, etc instead of stack traces full of garbage!
+        except Exception as exc:
+            logger.debug("Error while running:\n{}".format(sql))
+            logger.debug(exc)
+            raise dbt.exceptions.RuntimeException(dbt.compat.to_string(exc))
+
+    @classmethod
+    def open(cls, connection):
+        if connection.state == 'open':
+            logger.debug('Connection is already open, skipping open.')
+            return connection
+
+        credentials = connection.credentials
+
+        # it's impossible for presto to fail here as 'connections' are actually
+        # just cursor factories.
+        handle = presto.connect(
+            host=credentials.host,
+            port=credentials.get('port', 8080),
+            username=credentials.get('username'),
+            password=credentials.get('password'),
+            catalog=credentials.database
+        )
+        connection.state = 'open'
+        connection.handle = handle
+        return connection
+
+    @classmethod
+    def get_status(cls, cursor):
+        return 'OK'
+
+    def cancel(self, connection):
+        pass
